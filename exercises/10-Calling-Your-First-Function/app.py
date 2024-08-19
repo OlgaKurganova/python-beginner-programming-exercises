a4e130dd39a966034c09b9f77bf77cfc21742008
Olga Kurganova <157152724+OlgaKurganova@users.noreply.github.com>
2024-08-19 18:18:51 +0000

    Calling_Your_First_Function

--- exercises/10-Calling-Your-First-Function/app.py
+++ exercises/10-Calling-Your-First-Function/app.py
@@ -4,3 +4,11 @@ def is_odd(my_number):
 
 def my_main_code():
     # ✅ ↓ Your code here ↓ ✅
+	
+ 
+
+    result = is_odd(45345)  # Llamar a la función is_odd con el número 45345
+    print(result)  # Imprimir el resultado en la consola
+
+# Llamar a la función my_main_code para ejecutar el código
+my_main_code()
