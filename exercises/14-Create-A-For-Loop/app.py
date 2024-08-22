7b2b1e914136cbba9b834f9f929f3b158ce4e0f2
Olga Kurganova <157152724+OlgaKurganova@users.noreply.github.com>
2024-08-22 16:33:21 +0000

    For_Loop

--- exercises/14-Create-A-For-Loop/app.py
+++ exercises/14-Create-A-For-Loop/app.py
@@ -1,5 +1,9 @@
 def standards_maker():
     # ✅↓ Write your code here ↓✅
-
+    for i in range(300):
+	     print("Haré preguntas si estoy atascado")
+	
 
 # ✅↓ remember to call the function outside (here) ↓✅
+
+standards_maker()
