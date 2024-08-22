7882a355547537b4a3c650b057d0a25454013fdd
Olga Kurganova <157152724+OlgaKurganova@users.noreply.github.com>
2024-08-22 16:10:27 +0000

    generate_random

--- exercises/11-Create-A-New-Function/app.py
+++ exercises/11-Create-A-New-Function/app.py
@@ -1,3 +1,7 @@
 import random
 
 # ✅↓ Write your code here ↓✅
+def generate_random():
+  return random.randint(0, 9)
+# Llamar a la función y imprimir el resultado
+print("Random number between 0 and 9 is:", generate_random())
